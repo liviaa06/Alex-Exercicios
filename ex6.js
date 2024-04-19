70e115eacdabdc2b7594743bf61583bd6d703aee
Livia <128159882+liviaa06@users.noreply.github.com>
2024-04-19 09:53:23 -0300

    Add files via upload

---
+++ ex6.js
@@ -0,0 +1,5 @@
+let musicas = ["rock", "sertanejo", "pop", "funk"];
+
+for (let i = 0; i < musicas.length; i++) {
+    console.log(musicas[i]);
+}
---
+++ ex7.js
@@ -0,0 +1,16 @@
+let produto = {
+
+    titulo: "Pqueno Principe",
+    autor: "Giovanna Sibim",
+    numeroDePaginas: "50",
+    editora: "harper",
+    isbn: "9788854172388"
+
+};
+
+console.log(`Livro:
+${produto.titulo},
+${produto.autor},
+${produto.numeroDePaginas},
+${produto.editora},
+${produto.isbn}`);
